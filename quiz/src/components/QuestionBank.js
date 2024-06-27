bf2601cdcc10e88ff946d53cd740825a21886714
yachi agrawal <yachiagrawal139@gmail,com>
2024-06-27 18:08:20 +0530

    first commit

---
+++ quiz/src/components/QuestionBank.js
@@ -0,0 +1,78 @@
+// QuestionBank.js
+
+const qBank = [
+	{
+		id: 1,
+		question: "What is the capital of Haryana?",
+		options: ["Yamunanagar", "Panipat", "Gurgaon", "Chandigarh"],
+		answer: "Chandigarh",
+	},
+	{
+		id: 2,
+		question: "What is the capital of Punjab?",
+		options: ["Patiala", "Ludhiana", "Amritsar", "Chandigarh"],
+		answer: "Chandigarh",
+	},
+	{
+		id: 3,
+		question: "What is the capital of India?",
+		options: ["Delhi", "Mumbai", "Kolkata", "Chennai"],
+		answer: "Delhi"
+	},
+	{
+		id: 4,
+		question: "What is the capital of Uttarakhad?",
+		options: ["Roorkee", "Haridwar", "Dehradun", "Nanital"],
+		answer: "Dehradun"
+	},
+	{
+		id: 5,
+		question: "What is capital of Uttar Pradesh?",
+		options: ["GB Nagar", "Lucknow", "Prayagraj", "Agra"],
+		answer: "Lucknow"
+	},
+    {
+        id: 6,
+		question: "Which of the three banks will be merged with the other two to create India’s third-largest bank?",
+		options: ["Punjab National Bank", "Indian Bank", "Bank of Baroda ", "Dena Bank"],
+		answer: "Indian Bank"
+	},
+    {
+        id: 7,
+		question: "What is the name of the weak zone of the earth’s crust?",
+		options: ["Seismic ", "Cosmic", "Formic", " Anaemic"],
+		answer: "Seismic"
+	},
+    {
+        id: 8,
+		question: "Where was India’s first national Museum opened?",
+		options: ["Delhi", "Rajasthan", "Hyderabad", "Mumbai"],
+		answer: "Mumbai"
+	},
+    {
+        id: 9,
+		question: " In 2019, Which popular singer was awarded the Bharat Ratna award?",
+		options: ["Lata Mangeshkar", " Asha Bhosle", " Bhupen Hazarika ", "Manna Dey"],
+		answer: " Bhupen Hazarika "
+	},
+    {
+        id: 10,
+		question: "The world’s nation 5G mobile network was launched by which country?",
+		options: ["Japan", "Asia", "South korea", "Malaysia"],
+		answer: "South korea"
+	},
+    {
+        id: 11,
+		question: "When was Pravasi Bhartiya Divas held in Varanasi?",
+		options: ["2017", "2015", "2019", "2020"],
+		answer: "2019"
+	},
+    {
+        id: 12,
+		question: "Vijay Singh (golf player) is from which country?",
+		options: ["Uk", "Usa", "India", "Fiji"],
+		answer: "Fiji"
+	},
+]
+
+export default qBank;
